141e5c00c94584c55536ead56f56667477bc8aa0
Jesus251101 <61483092+Jesus251101@users.noreply.github.com>
2020-02-25 20:14:54 -0600

    Delete 1.py

--- 1.py
+++
@@ -1,5 +0,0 @@
-print("{:5}{}".format("Name,""Jesus David"))
-print("{:5}{}".format("Age","18 years"))
-print("{:5}{}".format("Height","1.65m"))
-print("{:5}{}".format("Driver´s Lincense","False"))
-
