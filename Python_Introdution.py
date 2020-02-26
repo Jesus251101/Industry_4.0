30aeeeeafd09123e984b73246087d0d0de1764ea
Jesus Juarez <61483092+Jesus251101@users.noreply.github.com>
2020-02-25 21:53:29 -0600

    Add files via upload

---
+++ Python_Introdution.py
@@ -0,0 +1,7 @@
+print("{:5}{}".format("Name: ", "Jesus Juarez"))
+print("{:5}{}".format("Age: ", "18 years"))
+print("{:5}{}".format("Height: ", "1.65"))
+print("-----------------------")
+print("{:5}{}".format("Drivers´s License: : ", "False"))
+print("-----------------------")
+print("{:5}{}".format("ID: ", "193111675"))
